acc2d12bce1b7f821a78347e335c31a519ff366d
Samir Deeb <samirndeeb@gmail.com>
2019-08-15 14:27:32 +0000

    Figaro installation, Geocode added

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :check_user_permission, only: [:edit, :update, :destroy]
 
   def index
     @places = Place.order("name").page(params[:page]).per_page(10)
@@ -19,23 +21,12 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find(params[:id])
   end
 
   def edit
-    @place = Place.find(params[:id])
-
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
   end
 
   def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-
     @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
@@ -45,16 +36,21 @@ class PlacesController < ApplicationController
   end
 
   def destroy
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-
     @place.destroy
     redirect_to root_path
   end
 
   private
+
+  def check_user_permission
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+  end
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
   def place_params
     params.require(:place).permit(:name, :description, :address)

a3eaef0a374d4e8cf6600f6330e8d9ae4ac7d634
Samir Deeb <samirndeeb@gmail.com>
2019-09-02 18:07:58 +0000

    Email Sent

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "samir.deeb@yahoo.com", subject: "A comment has been added to your place")
+    mail(to: "samirndeeb@gmail.com", subject: "A comment has been added to your place")
   end
 end
